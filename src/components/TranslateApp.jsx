c1b11c1d25626cf6fa18c216b3d70bded9af9854
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-08-20 16:10:57 +0100

    Set up basic state management (storing input text, selected languages, and translated output).

--- src/components/TranslateApp.jsx
+++ src/components/TranslateApp.jsx
@@ -1,9 +1,45 @@
+import { useReducer } from "react";
 import logo from "../assets/logo.svg";
 import TranslateForm from "./TranslateForm";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "update_text_to_translate": {
+      return {
+        ...state,
+        toTranslate: action.newText,
+      };
+    }
+
+    case "update_translating_from": {
+      return {
+        ...state,
+        from: action.value,
+      };
+    }
+
+    case "update_translating_to": {
+      return {
+        ...state,
+        to: action.value,
+      };
+    }
+  }
+  throw Error("Unknown action: " + action.type);
+}
+
+const initialState = {
+  toTranslate: "Hello, how are you?",
+  from: "en",
+  to: "fr",
+  translatedText: "Bonjour, comment allez-vous ?",
+};
+
 const TranslateApp = () => {
+  const [translateAppState, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <main className="space-y-12 bg-[url(/hero_img-sm.jpg)] bg-top bg-no-repeat px-4 py-12 md:bg-[url(/hero_img.jpg)]">
+    <main className="min-h-screen space-y-12 bg-[url(/hero_img-sm.jpg)] bg-top bg-no-repeat px-4 py-12 md:bg-[url(/hero_img.jpg)] lg:bg-size-[100%_auto]">
       <div>
         <img
           src={logo}
@@ -13,7 +49,13 @@ const TranslateApp = () => {
           className="mx-auto"
         />
       </div>
-      <TranslateForm />
+      <TranslateForm
+        toTranslate={translateAppState.toTranslate}
+        alreadyTranslated={translateAppState.translatedText}
+        translatingFrom={translateAppState.from}
+        translatingTo={translateAppState.to}
+        dispatch={dispatch}
+      />
     </main>
   );
 };
